18ea48373c96062cfb9a6dbf9796810e810ef6fd
Dakota Sitton <dakotasitton@gmail.com>
2020-07-27 23:21:22 -0500

    Created JS file

---
+++ combatclicks.js
@@ -0,0 +1,9 @@
+class Hero {
+    constructor(name, health, level, clicksperturn, turnlimit) {
+        this.name = name;
+        this.health = health;
+        this.level = level;
+        this.clicksperturn = clicksperturn;
+        this.turnlimit = turnlimit;
+    }
+}
