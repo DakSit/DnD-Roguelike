b3adce7425185e51d32b1764ccfac3d9dc8ba65d
Dakota Sitton <dakotasitton@gmail.com>
2020-07-29 13:01:48 -0500

    Introduced JQuery, added class buttons, introduced flexbox

--- combatclicks.js
+++ combatclicks.js
@@ -1,9 +1,14 @@
 class Hero {
-    constructor(name, health, level, clicksperturn, turnlimit) {
+    constructor(name, className, health, level, clicksperturn, turnlimit) {
         this.name = name;
+        this.className = className;
         this.health = health;
         this.level = level;
         this.clicksperturn = clicksperturn;
         this.turnlimit = turnlimit;
     }
+}
+
+function howToPlay(){
+  alert("Welcome! Click a class, click a room, and start clicking!");
 }
