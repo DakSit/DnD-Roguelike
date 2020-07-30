a6fd7738045a0ec559593d5086081db1140c87a3
Dakota Sitton <dakotasitton@gmail.com>
2020-07-29 22:10:17 -0500

    Multiple updates including jQuery actual use

--- combatclicks.js
+++ combatclicks.js
@@ -12,3 +12,53 @@ class Hero {
 function howToPlay(){
   alert("Welcome! Click a class, click a room, and start clicking!");
 }
+
+function classHide() {
+  var x = document.getElementById("options");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function classSelection(classSelection){
+  switch(classSelection) {
+  case "crusher":
+    classCrusher();
+    break;
+  case "catalyst":
+    classCatalyst();
+    break;
+  default:
+  classCretin();
+    // code block
+}
+  showClass();
+  classHide();
+  displayStats();
+}
+
+function classCrusher() {
+  player = new Hero("D", "crusher", 30, 1, 5, 5);
+}
+
+function classCatalyst() {
+  player = new Hero("D", "crusher", 30, 1, 5, 5);
+}
+
+function classCretin() {
+  player = new Hero("D", "crusher", 30, 1, 5, 5);
+}
+
+$(document).ready(function(){
+$("#optionButton").click(function(){
+  $("#characterStats").show();
+
+});})
+
+$(document).ready(function(){
+$("#optionButton").click(function(){
+  $("#options").hide();
+
+});})
