c53cc0b1cf2553cdde5ac7fa4ff1780d3ef8e298
Dakota Sitton <dakotasitton@gmail.com>
2020-07-30 21:02:28 -0500

    Created ability for JS variables to apply to page.

--- combatclicks.js
+++ combatclicks.js
@@ -13,52 +13,54 @@ function howToPlay(){
   alert("Welcome! Click a class, click a room, and start clicking!");
 }
 
-function classHide() {
-  var x = document.getElementById("options");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+function classCrusher() {
+  player = new Hero("D", "crusher", 30, 1, 15, 15);
+   document.getElementById('playerName').innerHTML = player.name;
+   document.getElementById('playerClass').innerHTML = player.className;
+   document.getElementById('playerHealth').innerHTML = player.health;
+   document.getElementById('playerLevel').innerHTML = player.level;
+   document.getElementbyId('playercpt').innerHTML = player.clicksperturn;
+   document.getElementbyId('playertl').innerHTML = player.turnlimit;
 }
 
-function classSelection(classSelection){
-  switch(classSelection) {
-  case "crusher":
-    classCrusher();
-    break;
-  case "catalyst":
-    classCatalyst();
-    break;
-  default:
-  classCretin();
-    // code block
+function classCatalyst() {
+  player = new Hero("D", "catalyst", 30, 1, 5, 5);
 }
+
+function classCretin() {
+  player = new Hero("D", "cretin", 30, 1, 5, 5);
+}
+
+
+
+function classSelection(name){
+  if (name==1)
+  {
+    classCrusher();
+  }
+  else if(name==2)
+  {
+    classCatalyst();
+  }
+  else {
+    classCretin();
+  }
   showClass();
   classHide();
   displayStats();
 }
 
-function classCrusher() {
-  player = new Hero("D", "crusher", 30, 1, 5, 5);
-}
-
-function classCatalyst() {
-  player = new Hero("D", "crusher", 30, 1, 5, 5);
-}
-
-function classCretin() {
-  player = new Hero("D", "crusher", 30, 1, 5, 5);
-}
 
 $(document).ready(function(){
-$("#optionButton").click(function(){
+$(".optionButton").click(function(){
   $("#characterStats").show();
 
 });})
 
 $(document).ready(function(){
-$("#optionButton").click(function(){
+$(".optionButton").click(function(){
   $("#options").hide();
 
 });})
+
+//document.getElementById("playerHealth").innerHTML =  player.health;
